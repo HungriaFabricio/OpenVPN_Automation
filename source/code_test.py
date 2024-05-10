960eb6f8d4719253328b5b11134b8e35865a178d
hungriafabricio <blueishot4@gmail.com>
2024-05-10 18:39:33 -0300

    Last changes to create VPN

--- source/code_test.py
+++ source/code_test.py
@@ -1,28 +1,18 @@
-#!/usr/bin/env python3
+# %%
 
-import subprocess
-import os, time
+while True:
+    final = input("A VPN foi criada com sucesso? (Y/N): ").strip().upper()  # Remover espaços em branco e converter para maiúsculas
+    
+    if final == "Y" or "sim" or "yes":
+        print("VPN criada com sucesso =)")
+        break  # Sai do loop se a resposta for "Y"
+    elif final == "N" or "nao" or "no":
+        print("Algum erro ocorreu no código, contate o time de infraestrutura =(")
+        break  # Sai do loop se a resposta for "N"
+    else:
+        print("Valor inválido, tente novamente usando Y ou N")
+# %%
 
-user_VPN = input("Escolha o nome do usuário da VPN ")
-password_VPN = input("Escolha a senha do usuário da VPN ")
+# %%
 
-# Entrar no diretório easy-rsa
-
-first_command = os.chdir("/home/openvpn/easy-rsa/")
-
-# Criação do Certificado
-
-second_command  = "./easyrsa gen-req {user} pass".format(user = user_VPN)
-second_result = subprocess.run(second_command, shell=True)
-
-time.sleep(2)
-
-fourth_command = "./easyrsa sign-req client {user}".format(user = user_VPN)
-fourth_result = subprocess.run(fourth_command, shell=True)
-
-# Criação do TLS Crypt V2
-
-fifth_command = os.chdir("/home/openvpn/easy-rsa/pki/")
-
-sixth_command = "openvpn --tls-crypt-v2 private/vpn_server.pem --genkey tls-crypt-v2-client private/{user}.pem".format(user = user_VPN)
-sixth_result = subprocess.run(sixth_command, shell=True)
+# %%
--- source/openvpn_usercreation.py
+++ source/openvpn_usercreation.py
@@ -4,7 +4,6 @@ import subprocess
 import os, time
 
 user_VPN = input("Escolha o nome do usuário da VPN ")
-password_VPN = input("Escolha a senha do usuário da VPN ")
 
 print("Entrar no diretório easy-rsa")
 
@@ -66,13 +65,18 @@ sixteenth_result = subprocess.run(sixteenth_command, shell=True)
 
 seventeenth_command = os.chdir("/home/openvpn/vpn_clients/{user}".format(user = user_VPN))
 
-eighteenth_command = "mv ./Default.ovpn {user}.ovpn".format(user = user_VPN)
+eighteenth_command = "mv Default.ovpn {user}.ovpn".format(user = user_VPN)
 
 print("Pause")
 
-final = input("O arquivo foi conclúido? Y/N ")
-
-if final == "Y":
-    print("VPN criada com sucesso!")
-else:
-    print("Algum erro ocorreu no código, contate o time de infraestrutura =)")
+while True:
+    final = input("A VPN foi criada com sucesso? (Y/N): ").strip().upper()  # Remover espaços em branco e converter para maiúsculas
+    
+    if final == "Y" or "sim" or "yes":
+        print("VPN criada com sucesso =)")
+        break  # Sai do loop se a resposta for "Y"
+    elif final == "N" or "nao" or "no":
+        print("Algum erro ocorreu no código, contate o time de infraestrutura =(")
+        break  # Sai do loop se a resposta for "N"
+    else:
+        print("Valor inválido, tente novamente usando Y ou N")
